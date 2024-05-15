b49100628b0377862908e9da58f3ef5514479a95
Harry Leach <harry@pluscosmic.dev>
2024-05-15 22:50:36 +0100

    Remember volume, added an option to save settings in memory and flush to disk on exit so we're not reading and writing to disk every time the volume is adjusted

--- ClipsArchiver/Entities/Settings.cs
+++ ClipsArchiver/Entities/Settings.cs
@@ -5,6 +5,7 @@ public class Settings
     public string ClipsPath { get; set; } = string.Empty;
     public int UserId { get; set; }
     public bool ShouldWatchClipsPath { get; set; }
+    public int? VideoVolume { get; set; }
 
     public static Settings GetDefaultSettings()
     {
@@ -12,7 +13,8 @@ public class Settings
         {
             ClipsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyVideos) + @"\Clips\Apex Legends",
             UserId = 0,
-            ShouldWatchClipsPath = false
+            ShouldWatchClipsPath = false,
+            VideoVolume = 100
         };
     }
 }
